56266eecb850658083ad8468b64d611957b8c520
Muhammad Anas <anastts.pk@gmail.com>
2015-03-09 01:44:18 +0500

    Uncomment file downloading and unzipping commands in plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
-## download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", method="curl", destfile="household_power_consumption.zip")
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", method="curl", destfile="household_power_consumption.zip")
 
-## unzip("household_power_consumption.zip")
+unzip("household_power_consumption.zip")
 pcdata <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
 
 pcdata <- pcdata[pcdata$Date == "1/2/2007" | pcdata$Date == "2/2/2007",]
