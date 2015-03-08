18917a7ee246accfae0c71ae6c56f5b521bfdb4e
Muhammad Anas <anastts.pk@gmail.com>
2015-03-09 01:47:15 +0500

    Uncomment data downloading, unzipping, subsetting and transformation code in plot3.R

--- plot3.R
+++ plot3.R
@@ -1,13 +1,13 @@
-## download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", method="curl", destfile="household_power_consumption.zip")
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", method="curl", destfile="household_power_consumption.zip")
 
-## unzip("household_power_consumption.zip")
-# pcdata <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+unzip("household_power_consumption.zip")
+pcdata <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
 
-# pcdata <- pcdata[pcdata$Date == "1/2/2007" | pcdata$Date == "2/2/2007",]
+pcdata <- pcdata[pcdata$Date == "1/2/2007" | pcdata$Date == "2/2/2007",]
 
-# pcdata$Time <- paste(pcdata$Date, pcdata$Time)
-# pcdata$Date <- as.Date(pcdata$Date, format = "%d/%m/%Y")
-# pcdata$Time <- strptime(pcdata$Time, format="%d/%m/%Y %H:%M:%S")
+pcdata$Time <- paste(pcdata$Date, pcdata$Time)
+pcdata$Date <- as.Date(pcdata$Date, format = "%d/%m/%Y")
+pcdata$Time <- strptime(pcdata$Time, format="%d/%m/%Y %H:%M:%S")
 
 png(file="plot3.png")
 plot(pcdata$Time, pcdata$Sub_metering_1, col="black", type="l", ylab="Energy sub metering", xlab="", main="")
